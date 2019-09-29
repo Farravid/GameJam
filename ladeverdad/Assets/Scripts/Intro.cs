abe86fea9a8a52bd43117277174e3afad676befe
Farravid <videdavid@hotmail.com>
2019-09-29 10:29:58 +0200

    Intro TERMINADA DE VERDAD

--- ladeverdad/Assets/Scripts/Intro.cs
+++ ladeverdad/Assets/Scripts/Intro.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
 
 public class Intro : MonoBehaviour
+    
 {
+    public GameObject bruja;
+    public Text bolaText;
     // Start is called before the first frame update
     void Start()
     {
+        bruja.SetActive(false);
+        bolaText.gameObject.SetActive(false);
         StartCoroutine(espera());
     }
 
@@ -19,7 +27,11 @@ public class Intro : MonoBehaviour
 
     public IEnumerator espera()
     {
-        yield return new WaitForSeconds(6);
+        yield return new WaitForSeconds(5);
+        bolaText.gameObject.SetActive(true);
+        bruja.SetActive(true);
+        
+        yield return new WaitForSeconds(4);
         SceneManager.LoadScene("SampleScene");
     }
 }
