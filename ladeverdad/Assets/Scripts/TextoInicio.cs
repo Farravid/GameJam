21e8eb5f4ddaff6eef01bc6f00d38f22bd6603c1
OujyMaximus <oujy93@gmail.com>
2019-09-29 15:17:58 +0200

    Texto al inicio

---
+++ ladeverdad/Assets/Scripts/TextoInicio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextoInicio : MonoBehaviour
+{
+    public Text inicio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inicio.gameObject.SetActive(false);
+        StartCoroutine(Muestra());
+    }
+
+    public IEnumerator Muestra()
+    {
+        inicio.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3);
+        inicio.gameObject.SetActive(false);
+    }
+}
