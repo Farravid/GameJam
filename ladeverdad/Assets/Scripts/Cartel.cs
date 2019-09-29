7e77d3491f417e74a99251ff5ca3c1f6f7cd0389
Farravid <videdavid@hotmail.com>
2019-09-29 13:05:16 +0200

    Cosas Maravillosas final dungeon

---
+++ ladeverdad/Assets/Scripts/Cartel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cartel : MonoBehaviour
+{
+    public Text interact;
+    private bool dentro;
+    //public GameObject canvashostia;
+    private void Start()
+    {
+        interact.gameObject.SetActive(false);
+        dentro = false;
+    }
+    private void Update()
+    {
+
+        if (PauseMenu.GameIsPaused)
+        {
+            interact.gameObject.SetActive(false);
+        }
+        else if (PauseMenu.GameIsPaused == false && dentro)
+        {
+            interact.gameObject.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.E) && dentro)
+        {
+            interact.gameObject.SetActive(true);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.transform.tag == "Player")
+        {
+            interact.gameObject.SetActive(true);
+            dentro = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.transform.tag == "Player")
+        {
+            interact.gameObject.SetActive(false);
+            dentro = false;
+        }
+    }
+}
